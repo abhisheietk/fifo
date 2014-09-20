469206f394da6c4678954ed702053122c6c35825
Abhishek Bajpai <abhisheietk@gmail.com>
2014-09-20 12:30:03 +0530

    Scripted auto-commit on change (2014-09-20 12:30:03) by gitwatch.sh

--- hdl/dpram.v
+++ hdl/dpram.v
@@ -21,3 +21,27 @@ module dpram (
     input  [ADDR-1:0]   a_port_ADDR;
     input  [DATA-1:0]   a_port_data_IN;
     output [DATA-1:0]   a_port_data_OUT;
+    // Memory as multi-dimensional array
+    reg [DATA-1:0] Memory [0:2**ADDR-1];
+    
+    // Write data to Memory
+    always @(posedge clK) begin
+        if (a_port_WR) begin
+            Memory[a_port_ADDR] <= a_port_data_IN;
+        end
+    end
+    always @(posedge clK) begin
+        if (b_port_WR) begin
+            Memory[b_port_ADDR] <= b_port_data_IN;
+        end
+    end
+    
+    // Read data from memory
+    always @(posedge clK) begin
+        a_port_data_OUT <= Memory[a_port_ADDR];
+    end
+    always @(posedge clK) begin
+        b_port_data_OUT <= Memory[b_port_ADDR];
+    end
+
+endmodule
