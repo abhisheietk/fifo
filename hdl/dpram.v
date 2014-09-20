731a439a43564c3e6eb66c5ade3f5786758f15c8
Abhishek Bajpai <abhisheietk@gmail.com>
2014-09-20 12:42:19 +0530

    Scripted auto-commit on change (2014-09-20 12:42:19) by gitwatch.sh

--- hdl/dpram.v
+++ hdl/dpram.v
@@ -16,36 +16,14 @@ module dpram (
     parameter ADDR = 5;    input clK;
     
     // port A
-    input               a_port_WR;
-    input  [ADDR-1:0]   a_port_ADDR;
-    input  [DATA-1:0]   a_port_data_IN;
-    output [DATA-1:0]   a_port_data_OUT;
+    input  wire              a_port_WR;
+    input  wire [ADDR-1:0]   a_port_ADDR;
+    input  wire [DATA-1:0]   a_port_data_IN;
+    output wire [DATA-1:0]   a_port_data_OUT;
     
     // port B
-    input               b_port_WR;
-    input  [ADDR-1:0]   b_port_ADDR;
-    input  [DATA-1:0]   b_port_data_IN;
-    output [DATA-1:0]   b_port_data_OUT;
+    input  wire              b_port_WR;
+    input  wire [ADDR-1:0]   b_port_ADDR;
+    input  wire [DATA-1:0]   b_port_data_IN;
+    output wire [DATA-1:0]   b_port_data_OUT;
     
-    // Memory as multi-dimensional array
-    reg [DATA-1:0] Memory [0:2**ADDR-1];
-    
-    // Write data to Memory
-    always @(posedge clK) begin
-        if (a_port_WR) begin
-            Memory[a_port_ADDR] <= a_port_data_IN;
-        end
-    end
-    always @(posedge clK) begin
-        if (b_port_WR) begin
-            Memory[b_port_ADDR] <= b_port_data_IN;
-        end
-    end
-    
-    // Read data from memory
-    always @(posedge clK) begin
-        a_port_data_OUT <= Memory[a_port_ADDR];
-        b_port_data_OUT <= Memory[b_port_ADDR];
-    end
-
-endmodule
