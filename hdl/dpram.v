4109e85061fb5f554914e10939fdc91d1e8a5dea
Abhishek Bajpai <abhisheietk@gmail.com>
2014-09-20 12:18:28 +0530

    Scripted auto-commit on change (2014-09-20 12:18:28) by gitwatch.sh

---
+++ hdl/dpram.v
@@ -0,0 +1,11 @@
+module dpram (
+    clK, 
+    a_port_WR,
+    a_port_ADDR,
+    a_port_data_IN,
+    a_port_data_OUT,
+    b_port_WR,
+    b_port_ADDR,
+    b_port_data_IN,
+    b_port_data_OUT
+);
