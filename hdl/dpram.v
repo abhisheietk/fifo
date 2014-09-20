f1c67c9e948fc64ae7ef6ae00b6c352f2b803a55
Abhishek Bajpai <abhisheietk@gmail.com>
2014-09-20 12:30:13 +0530

    Scripted auto-commit on change (2014-09-20 12:30:13) by gitwatch.sh

--- hdl/dpram.v
+++ hdl/dpram.v
@@ -1,26 +1,31 @@
 module dpram (
     clK, 
+
     a_port_WR,
     a_port_ADDR,
     a_port_data_IN,
     a_port_data_OUT,
+
     b_port_WR,
     b_port_ADDR,
     b_port_data_IN,
     b_port_data_OUT
-);    input clK;
+);
+
+    parameter DATA = 16;
+    parameter ADDR = 5;    input clK;
     
     // port A
-    input              a_port_WR;
+    input               a_port_WR;
     input  [ADDR-1:0]   a_port_ADDR;
     input  [DATA-1:0]   a_port_data_IN;
     output [DATA-1:0]   a_port_data_OUT;
     
     // port B
-    input              a_port_WR;
-    input  [ADDR-1:0]   a_port_ADDR;
-    input  [DATA-1:0]   a_port_data_IN;
-    output [DATA-1:0]   a_port_data_OUT;
+    input               b_port_WR;
+    input  [ADDR-1:0]   b_port_ADDR;
+    input  [DATA-1:0]   b_port_data_IN;
+    output [DATA-1:0]   b_port_data_OUT;
     // Memory as multi-dimensional array
     reg [DATA-1:0] Memory [0:2**ADDR-1];
     
